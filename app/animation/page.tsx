ef7004c7296621205d7016fe7fcef4ddb2de3797
amit rawat <amitrawat4532@gmail.com>
2025-03-26 22:37:22 +0530

    new changes

---
+++ app/animation/page.tsx
@@ -0,0 +1,14 @@
+import { MacbookScroll } from '@/components/ui/macbook-scroll'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <section className=''>
+        <MacbookScroll />
+    </section>
+  )
+}
+
+export default page
